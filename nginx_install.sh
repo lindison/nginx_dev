26aae832d7738ceccb2c6dbf7118c4fef59ba1d2
Lindis <me@lindison.com>
2016-12-30 16:12:01 -0700

    updates

--- nginx_install.sh
+++ nginx_install.sh
@@ -2,8 +2,6 @@
 
 wget http://nginx.org/download/nginx-1.11.7.tar.gz
 tar zxf nginx-1.11.7.tar.gz
-mv nginx-1.11.7 /home/ubuntu
-cd /home/ubuntu/nginx-1.11.7 
 apt install -y build-essential libpcre3 libpcre3-dev libpcrecpp0v5 libssl-dev zlib1g-dev
 ./configure --sbin-path=/usr/bin/nginx \
             --conf-path=/etc/nginx/nginx.conf \
@@ -12,4 +10,5 @@ apt install -y build-essential libpcre3 libpcre3-dev libpcrecpp0v5 libssl-dev zl
             --with-debug --with-pcre \
             --with-http_ssl_module
 make
+rm -rf nginx-1.11.7
 rm -f /home/ubuntu/code/nginx-1.11.7.tar.gz
